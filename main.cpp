c754b58e69e7cf4944b408c1eff324e1ac485420
Steven <wxianxinreg@gmail.com>
2021-09-19 23:45:15 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 //      b. To get correct Epoch timestamp, the saved value needs to be converted.
 // 2. Did not complete the task of generating 5000 Tickers due to limited memory handling. Here is
 // using 100 tickers for an example.
+// 3. TradingData is the collection of all the tickers, each ticker has one TickerData
+// 4. TickerData is the collection of all the trading data for one ticker.
 
 #include <iostream>
 #include <string>
@@ -18,7 +20,7 @@ int main() {
     TradingData myData = TradingData(100);
 
     // Generate Randon Data
-    myData.generateRandomData(0, 0.1);
+    myData.generateRandomData(0, 0.1);   // mean = 0, sigma = 0.1, distribution of random variable
     Struct values = myData.ticker_list["S0001"].getValues(TIMESTAMP_START + 100 * 5);
     std::cout << "Tick Timestamp: " << values.tick << ", Tick bid: " << values.bid
               << ", Tick ask:" << values.ask << ", Tick bidSize:" << values.bidSize
